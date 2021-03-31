da69f42dfb8a1a72af5f97486f39371c04fcdc43
Prakriti Latawa <prakriti.latawa110@gmail.com>
2021-03-31 17:55:49 +0530

    ST

---
+++ index.js
@@ -0,0 +1,10 @@
+// function number(){
+//     var x;
+//     x = document.getElementById("lname").value;
+//     if(isNaN(x) || x<1 || x>10){
+//         alert("Please enter valid Mobile number");
+//     }
+// }
+
+// function nameLength(t){
+   
